1d9f40487b2bbe09af90b06fbca5e0637ac4b782
DEBAYAN BANDYOPADHYAY <debayan@cse.iitb.ac.in>
2018-09-28 13:45:03 +0530

    Version1 Codes:To be debugged

---
+++ Client.c
@@ -0,0 +1,137 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 8080 
+
+struct sockaddr_in address; 
+int sock = 0, valread; 
+struct sockaddr_in serv_addr; 
+char buffer[1024]={0};
+int conn=0;
+    
+void connect1(char *ip,char *port)
+{
+    if(conn==1)
+    {
+        printf("Error:You are already connected to a server...\nYou need to disconnect before connecting to another server\n");
+        return;
+    }
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return; 
+    } 
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(atoi(port)); 
+       
+    if(inet_pton(AF_INET,ip, &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n");
+        return; 
+    }
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return;
+    }
+    conn=1;
+}
+
+void disconnect()
+{
+    if(conn==0)
+    {
+        printf("Error:You are not connected to server...\nYou need to connect before disconnecting\n");
+        return;
+    }
+    char *s="disconnect";
+    send(sock,s,strlen(s),0);
+    shutdown(sock,2);
+    conn=0;
+    printf("OK");
+}
+
+void interactive()
+{
+    char s[4096],tokens[4][1000],*p;
+    int nos,i,c,a1,a2,turn;
+    while(1)
+    {   
+        nos=c=0;
+        fflush(stdin);
+        fgets(s,4096,stdin);
+        p=s;
+        for(i=0;i<strlen(s)-1;i++)
+        {
+            if(s[i]==' ')
+            {    
+                tokens[nos][c]='\0';
+                nos++;
+                c=0;
+            }
+            else
+                tokens[nos][c++]=s[i];
+        }
+        tokens[nos][c]='\0';
+        if(!strcmp(tokens[0],"connect") && nos==2)
+        {
+            connect1(tokens[1],tokens[2]);
+        }
+        else if(!strcmp(tokens[0],"disconnect") && nos==0)
+        {
+            printf("Disconnecting...\n");
+            disconnect();
+        }
+        else if(conn==1 && !strcmp(tokens[0],"create") && nos==3)
+        {
+            send(sock,s,strlen(s),0);
+            valread = read( sock , buffer, 1024); 
+            printf("%s\n",buffer );
+        }
+        else if(conn==1 && !strcmp(tokens[0],"read") && nos==1)
+        {
+            send(sock,s,strlen(s),0);
+            valread = read( sock , buffer, 1024); 
+            printf("%s\n",buffer );   
+        }
+        else if(conn==1 && !strcmp(tokens[0],"update") && nos==3)
+        {
+            send(sock,s,strlen(s),0);
+            valread = read( sock , buffer, 1024); 
+            printf("%s\n",buffer );   
+        }
+        else if(conn==1 && !strcmp(tokens[0],"delete") && nos==1)
+        {
+            send(sock,s,strlen(s),0);
+            valread = read( sock , buffer, 1024); 
+            printf("%s\n",buffer );
+        }
+        else if(conn==1 && !strcmp(tokens[0],"display") && nos==0)
+        {
+            send(sock,s,strlen(s),0);
+        }
+        else
+            printf("Incorrect arguments entered...\n");
+    } 
+    
+}
+   
+int main(int argv, char const *argc[]) 
+{ 
+    if(argv==2 && !strcmp(argc[1],"interactive"))
+    {
+        printf("You are operating in interactive mode...\n");
+        interactive();
+    }
+    else if(argv==3 && !strcmp(argc[1],"batch"))
+    {
+        printf("You are operating in batch mode...\n");
+    }
+    else
+        printf("Incorrect arguments passed\nExiting...\n");
+    return 0; 
+ 
+} 
---
+++ Server.c
@@ -0,0 +1,268 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<pthread.h>
+//THREAD VALUES
+int v1 = 1;
+int v2 = 2;
+int v3 = 3;
+int v4 = 4;
+//SERVER DETAILS
+int server_fd, new_socket, valread; 
+struct sockaddr_in address; 
+int opt = 1; 
+int addrlen = sizeof(address);
+char buffer[1024];
+//THE THREADS
+pthread_t t1,t2,t3,t4;
+//THE MUTEXS
+pthread_mutex_t mutex_table = PTHREAD_MUTEX_INITIALIZER;
+//THE CONDITIONAL VARIABLES
+pthread_cond_t busy= PTHREAD_COND_INITIALIZER;
+//HASH TABLE
+typedef struct hashtable
+{
+	int key;
+	char *value;
+	struct hashtable* next;
+}ht; 
+//HASH TABLE CREATED
+ht *table=NULL;
+
+ht* create(int key,int size,char *value)//CHECKED: create a new node
+{
+	ht *h=(ht*)malloc(sizeof(ht));
+	h->key=key;
+	h->value=(char*)malloc(sizeof(char)*size);
+	strcpy((h->value),value);
+	h->next=NULL;
+	return h;
+}
+
+int htsearch(int key)//CHECKED: checks whether key is present or not
+{
+	if(table==NULL)
+		return -1;
+	ht *p=table;
+	while(p!=NULL && p->key!=key)
+		p=p->next;
+	if(p!=NULL)
+		return 1;
+	else 
+		return -1;
+}
+
+char* hvalue(int key)//CHECKED: returns value corresponding to the key
+{
+	ht *p=table;
+	while(p->key!=key)
+		p=p->next;
+	return p->value;
+}
+
+void htappend(int key,int size,char* value)//CHECKED: Appends to the table
+{
+	ht *p=table;
+	if(table==NULL)
+	{
+		table=create(key,size,value);
+		return;
+	}
+	while(p->next!=NULL)
+		p=p->next;
+	p->next=create(key,size,value);
+}
+
+void delete(int key)//CHECKED: Deletes key value pair from table
+{
+	if(table->key==key)
+	{
+		ht *x=table;
+		table=table->next;
+		free(x);
+		return;
+	}
+	ht *p=table;
+	while(p->next->key!=key)
+		p=p->next;
+	ht *x=p->next;
+	p->next=p->next->next;
+	free(x);
+}
+
+void update(int key,int size,char* value)//CHECKED: Updates the key value pair
+{
+	ht *p=table;
+	while(p->key!=key)
+		p=p->next;
+	free(p->value);
+	p->value=(char*)malloc(size*sizeof(char));
+	strcpy(p->value,value);
+}
+
+void display()//CHECKED: Displays the contents
+{
+	ht *p=table;// p points to table
+	while(p!=NULL)
+	{
+		printf("key=%d value=%s\n",p->key,p->value);
+		p=p->next;
+	}
+}
+
+void *serve(void *data)//NOT CHECKED: Handle the requests 
+{
+	char tokens[4][1000],*msg;
+	int stillconnect=0,c,nos,i;
+	while(1)
+	{
+		int new_socket;
+		new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen);
+		stillconnect=1;
+		while(stillconnect)
+		{
+			read(new_socket , buffer, 1024);
+			c=nos=0;
+			free(msg);
+			msg=(char*)malloc(1024*sizeof(char));
+			printf("%s\n",buffer);
+			for(i=0;i<strlen(buffer)-1;i++)
+	        {
+	            if(buffer[i]==' ')
+	            {    
+	                tokens[nos][c]='\0';
+	                nos++;
+	                c=0;
+	            }
+	            else
+	                tokens[nos][c++]=buffer[i];
+	        }
+	        printf("%s",tokens[0]);
+			if(!strcmp(tokens[0],"disconnect"))
+				stillconnect=0;
+			else if(!strcmp(tokens[0],"create"))
+			{	
+				pthread_mutex_lock(&mutex_table);
+				if(htsearch(atoi(tokens[1]))==1)
+				{
+					msg="Error: Key already exists";
+					send(new_socket , msg , strlen(msg) , 0 );
+				}
+				else
+				{
+					htappend(atoi(tokens[1]),atoi(tokens[2]),tokens[3]);
+					msg="OK";
+					send(new_socket , msg , strlen(msg) , 0 );	
+				}
+				pthread_mutex_unlock(&mutex_table);
+			}
+			else if(!strcmp(tokens[0],"read"))
+			{	
+				pthread_mutex_lock(&mutex_table);
+				if(htsearch(atoi(tokens[1]))==-1)
+				{
+					msg="Error: Key does not exist";
+					send(new_socket , msg , strlen(msg) , 0 );
+				}
+				else
+				{
+					msg=hvalue(atoi(tokens[1]));
+					send(new_socket , msg , strlen(msg) , 0 );	
+				}
+				pthread_mutex_unlock(&mutex_table);
+			}
+			else if(!strcmp(tokens[0],"update"))
+			{
+				pthread_mutex_lock(&mutex_table);	
+				if(htsearch(atoi(tokens[1]))==-1)
+				{
+					msg="Error: Key does not exist";
+					send(new_socket , msg , strlen(msg) , 0 );
+				}
+				else
+				{
+					update(atoi(tokens[1]),atoi(tokens[2]),tokens[3]);
+					msg="OK";
+					send(new_socket , msg , strlen(msg) , 0 );	
+				}
+				pthread_mutex_unlock(&mutex_table);
+			}
+			else if(!strcmp(tokens[0],"delete"))
+			{
+				pthread_mutex_lock(&mutex_table);	
+				if(htsearch(atoi(tokens[1]))==-1)
+				{
+					msg="Error: Key does not exist";
+					send(new_socket , msg , strlen(msg) , 0 );
+				}
+				else
+				{
+					delete(atoi(tokens[1]));
+					msg="OK";
+					send(new_socket , msg , strlen(msg) , 0 );	
+				}
+				pthread_mutex_unlock(&mutex_table);
+			}
+			else if(!strcmp(tokens[0],"display"))
+			{
+				pthread_mutex_lock(&mutex_table);	
+				display();
+				pthread_mutex_unlock(&mutex_table);
+			}
+
+		}
+	}
+}
+
+void sockcreate(char *ip,char *port)//CHECKED: Creates the socket
+{
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        printf("Error: Socket not created"); 
+        exit(1); 
+    }    
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,&opt, sizeof(opt))) 
+    { 
+       	printf("Error:setsockopt setup failure"); 
+        exit(1); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons(atoi(port)); 
+    if (bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0) 
+    { 
+       	printf("Error:Binding failed"); 
+        exit(1); 
+    } 
+    if (listen(server_fd, 4) < 0) 
+    { 
+       	printf("Error:Failed to listen"); 
+        exit(1); 
+    }
+}
+
+int main(int argv,char* argc[])
+{
+	if(argv!=3)
+	{
+		printf("Error: Parameters missing\n");
+		return 0;
+	}
+     
+	sockcreate(argc[1],argc[2]);
+     
+    pthread_create(&t1, NULL,serve, (void *)&v1);
+    pthread_create(&t2, NULL,serve, (void *)&v2);
+    pthread_create(&t3, NULL,serve, (void *)&v3);
+    pthread_create(&t4, NULL,serve, (void *)&v4);
+    
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    pthread_join(t3,NULL);
+    pthread_join(t4,NULL);
+    
+    return 0;
+}
